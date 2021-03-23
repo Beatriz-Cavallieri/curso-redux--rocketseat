a6805a66c0ccd827ecde5b503100054b216297a9
Beatriz Cavallieri <bfcavallieri@gmail.com>
2021-03-22 22:45:58 -0300

    Corrigir adiçao a partir do segundo item de um produto e utilizar o immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,29 +1,35 @@
-import { Reducer } from "redux"
-import { ICartState } from "./types"
+import { Reducer } from "redux";
+import { ICartState } from "./types";
+import produce from "immer";
 
 const INITIAL_STATE: ICartState = {
-    items: []
-}
+  items: [],
+};
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+  return produce(state, (draft) => {
     switch (action.type) {
-        case 'ADD_PRODUCT_TO_CART':
-            const { product } = action.payload
+      case "ADD_PRODUCT_TO_CART":
+        const { product } = action.payload;
 
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {
-                        product,
-                        quantity: 1,
-                    }
-                ]
-            }
+        const productInCartIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        );
 
-        default:
-            return state
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
+        break;
+
+      default:
+        return draft;
     }
-}
+  });
+};
 
-export default cart
+export default cart;
